be050bdb5b7d2ae94cb2466fca25b64e9fbc9dd5
Haradovich619 <ivan.gorodovich.99@mail.ru>
2023-04-27 18:52:50 +0300

    finish

--- quotes.toscrape.py
+++ quotes.toscrape.py
@@ -1,1 +1,45 @@
-print('my favorite team is manchester united')
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+def scrape_page(soup, quotes):
+    quote_elements = soup.find_all('div', class_='quote')
+    for quote_element in quote_elements:
+        text = quote_element.find('span', class_='text').text
+        author = quote_element.find('small', class_='author').text
+        tag_elements = quote_element.find('div', class_='tags').find_all('a', class_='tag')
+        tags = []
+        for tag_element in tag_elements:
+            tags.append(tag_element.text)
+        quotes.append(
+            {
+                'text': text,
+                'author': author,
+                'tags': ', '.join(tags)
+            }
+        )
+
+base_url = 'https://quotes.toscrape.com'
+page = requests.get(base_url)
+soup = BeautifulSoup(page.text, 'html.parser')
+quotes = []
+
+scrape_page(soup, quotes)
+
+next_li_element = soup.find('li', class_='next')
+
+while next_li_element is not None:
+    next_page_relative_url = next_li_element.find('a', href=True)['href']
+    page = requests.get(base_url + next_page_relative_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    scrape_page(soup, quotes)
+    next_li_element = soup.find('li', class_='next')
+
+csv_file = open('quotes.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(csv_file)
+writer.writerow(['Text', 'Author', 'Tags'])
+for quote in quotes:
+    writer.writerow(quote.values())
+csv_file.close()
+
+
